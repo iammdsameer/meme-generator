62ee40fdeb77ecbdb8eddcb41a5980351279b3a1
Sameer <itsmdsameerkhan@gmail.com>
2020-06-01 16:32:33 +0545

    Updates

---
+++ precache-manifest.a6da061a2dd3ea4a9311fbe1cfd61532.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007c6ea311a9c70055933a6e0d99d507",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "0d5cc6477c4b3f05a151",
+    "url": "/meme-generator/static/css/main.7ddfc823.chunk.css"
+  },
+  {
+    "revision": "13b1237dbaeb5b413912",
+    "url": "/meme-generator/static/js/2.070775ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.070775ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d5cc6477c4b3f05a151",
+    "url": "/meme-generator/static/js/main.9dd4c336.chunk.js"
+  },
+  {
+    "revision": "d61cbcd042e901af7906",
+    "url": "/meme-generator/static/js/runtime-main.df969e4a.js"
+  }
+]);
